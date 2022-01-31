c6bf09784152015d8cd2b7d75b68a2fd1e0cb5b5
hd-zhao-uu <haodong.zhao.uu@outlook.com>
2022-01-31 22:08:13 +0100

    format ped_model.cpp

--- libpedsim/ped_model.cpp
+++ libpedsim/ped_model.cpp
@@ -6,147 +6,141 @@
 // Adapted for Low Level Parallel Programming 2017
 //
 #include "ped_model.h"
-#include "ped_waypoint.h"
-#include "ped_model.h"
+#include <omp.h>
+#include <algorithm>
 #include <iostream>
 #include <stack>
-#include <algorithm>
-#include "cuda_testkernel.h"
-#include <omp.h>
 #include <thread>
+#include "cuda_testkernel.h"
+#include "ped_model.h"
+#include "ped_waypoint.h"
 
 #include <stdlib.h>
 
-void Ped::Model::setup(std::vector<Ped::Tagent*> agentsInScenario, std::vector<Twaypoint*> destinationsInScenario, IMPLEMENTATION implementation)
-{
-	// Convenience test: does CUDA work on this machine?
-	cuda_test();
+void Ped::Model::setup(std::vector<Ped::Tagent*> agentsInScenario,
+                       std::vector<Twaypoint*> destinationsInScenario,
+                       IMPLEMENTATION implementation) {
+    // Convenience test: does CUDA work on this machine?
+    cuda_test();
 
-	// Set 
-	agents = std::vector<Ped::Tagent*>(agentsInScenario.begin(), agentsInScenario.end());
+    // Set
+    agents = std::vector<Ped::Tagent*>(agentsInScenario.begin(),
+                                       agentsInScenario.end());
 
-	// Set up destinations
-	destinations = std::vector<Ped::Twaypoint*>(destinationsInScenario.begin(), destinationsInScenario.end());
+    // Set up destinations
+    destinations = std::vector<Ped::Twaypoint*>(destinationsInScenario.begin(),
+                                                destinationsInScenario.end());
 
-	// Sets the chosen implemenation. Standard in the given code is SEQ
-	this->implementation = implementation;
+    // Sets the chosen implemenation. Standard in the given code is SEQ
+    this->implementation = implementation;
 
-	// Set up heatmap (relevant for Assignment 4)
-	setupHeatmapSeq();
+    // Set up heatmap (relevant for Assignment 4)
+    setupHeatmapSeq();
 }
 
 // Utilities for Assignment 1
 // void a1Move (Ped::Tagent* agent) {
 // 	// retrieve the agent and calculate its next desired position
 // 	agent->computeNextDesiredPosition();
-		
+
 // 	int dX = agent->getDesiredX(), dY = agent->getDesiredY();
 // 	// set its position to the calculated desired one
 // 	agent->setX(dX);
 // 	agent->setY(dY);
 // };
 void pFunc(int tId, int start, int end, std::vector<Ped::Tagent*>& agents) {
-	// printf("thread= %d, start=%d, end=%d\n", tId, start, end);
-	for(int i = start; i <= end; i++) {
-		// a1Move(agent[i]);	
-		// retrieve the agent and calculate its next desired position
-		Ped::Tagent* agent = agents[i];
-		agent->computeNextDesiredPosition();
-			
-		int dX = agent->getDesiredX(), dY = agent->getDesiredY();
-		// set its position to the calculated desired one
-		agent->setX(dX);
-		agent->setY(dY);
-	}
-};	
+    // printf("thread= %d, start=%d, end=%d\n", tId, start, end);
+    for (int i = start; i <= end; i++) {
+        // a1Move(agent[i]);
+        // retrieve the agent and calculate its next desired position
+        Ped::Tagent* agent = agents[i];
+        agent->computeNextDesiredPosition();
+
+        int dX = agent->getDesiredX(), dY = agent->getDesiredY();
+        // set its position to the calculated desired one
+        agent->setX(dX);
+        agent->setY(dY);
+    }
+};
 
 void Ped::Model::tick() {
-    // EDIT HERE FOR ASSIGNMENT 1	
-	auto a1Move = [](Ped::Tagent* agent) {
-		// retrieve the agent and calculate its next desired position
-		agent->computeNextDesiredPosition();
-			
-		int dX = agent->getDesiredX(), dY = agent->getDesiredY();
-		// set its position to the calculated desired one
-		agent->setX(dX);
-		agent->setY(dY);
-	};	
+    // EDIT HERE FOR ASSIGNMENT 1
+    auto a1Move = [](Ped::Tagent* agent) {
+        // retrieve the agent and calculate its next desired position
+        agent->computeNextDesiredPosition();
 
-	// lambda function for threads
-	// auto pFunc = [&](int tId, int start, int end) {
-	// 	printf("thread= %d, start=%d, end=%d\n", tId, start, end);
-	// 	for(int i = start; i <= end; i++) {
-	// 		// a1Move(agent[i]);	
-	// 		a1Move(agents[i]);
-	//  	}
-	// };
-		
-	int agentSize = agents.size();
-	int threadNum = getThreadNum();
+        int dX = agent->getDesiredX(), dY = agent->getDesiredY();
+        // set its position to the calculated desired one
+        agent->setX(dX);
+        agent->setY(dY);
+    };
+
+    // lambda function for threads
+    // auto pFunc = [&](int tId, int start, int end) {
+    // 	printf("thread= %d, start=%d, end=%d\n", tId, start, end);
+    // 	for(int i = start; i <= end; i++) {
+    // 		// a1Move(agent[i]);
+    // 		a1Move(agents[i]);
+    //  	}
+    // };
+
+    int agentSize = agents.size();
+    int threadNum = getThreadNum();
     switch (implementation) {
-        case SEQ:
-			{	
-				// std::cout << "[SERIAL]" << std::endl;
-				for (int i = 0; i < agentSize; i++) {
-					// Ped::Tagent* agent = agents[i];
-					// a1Move(agent);
-					a1Move(agents[i]);
-				}
-			}
-			break;
-        case PTHREAD:
-			{	
-				// std::cout << "[USING PTHREADS]" << std::endl;
-				// printf("thread: %d\n", threadNum);
-				// printf("Agent size: %d\n", agentSize);
-				int agentsPerThread = agentSize / threadNum;
-				int agentLeft = agentSize % threadNum;
+        case SEQ: {
+            // std::cout << "[SERIAL]" << std::endl;
+            for (int i = 0; i < agentSize; i++) {
+                // Ped::Tagent* agent = agents[i];
+                // a1Move(agent);
+                a1Move(agents[i]);
+            }
+        } break;
+        case PTHREAD: {
+            // std::cout << "[USING PTHREADS]" << std::endl;
+            // printf("thread: %d\n", threadNum);
+            // printf("Agent size: %d\n", agentSize);
+            int agentsPerThread = agentSize / threadNum;
+            int agentLeft = agentSize % threadNum;
 
-				std::thread* threads = new std::thread[threadNum];
-				int start, end;
-				// allocation strategy:
-				// 	if `agentSize` cannot be evenly divided by `threadNum`, then
-				// 	the first `agentLeft` threads will handle 1 more agent than other the threads.
-				for(int i = 0; i < threadNum; i++) {	
-					if(i < agentLeft){
-						start = i * agentsPerThread + i;
-						end = start + agentsPerThread;
-					}
-					else {
-						start = i * agentsPerThread + agentLeft;
-						end = start + agentsPerThread - 1;
-					}
-					
-					threads[i] = std::thread(pFunc, i, start, end, std::ref(agents));
-					// threads[i] = std::thread(pFunc, i, start, end);
-				}
+            std::thread* threads = new std::thread[threadNum];
+            int start, end;
+            // allocation strategy:
+            // 	if `agentSize` cannot be evenly divided by `threadNum`, then
+            // 	the first `agentLeft` threads will handle 1 more agent than
+            // other the threads.
+            for (int i = 0; i < threadNum; i++) {
+                if (i < agentLeft) {
+                    start = i * agentsPerThread + i;
+                    end = start + agentsPerThread;
+                } else {
+                    start = i * agentsPerThread + agentLeft;
+                    end = start + agentsPerThread - 1;
+                }
 
-				for(int i = 0; i < threadNum; i++) {
-					threads[i].join();
-				}
+                threads[i] =
+                    std::thread(pFunc, i, start, end, std::ref(agents));
+            }
 
-				delete[] threads;
-				
-			}
-			break;
+            for (int i = 0; i < threadNum; i++) {
+                threads[i].join();
+            }
 
-        case OMP:
-			{	
-				// std::cout << "[USING OPENMP]" << std::endl;
-				// printf("thread: %d\n", threadNum);
-				int i;
-				#pragma omp parallel for shared(agents) num_threads(threadNum) schedule(static)
-				for (i = 0; i < agentSize; i++) {
-					// int nthrds = omp_get_num_threads();
-					// printf("omp threads: %d\n", nthrds);
-					// Ped::Tagent* agent = agents[i];
-					// a1Move(agent);
-					a1Move(agents[i]);
-				}
-				
-			}
-			break;	
-           
+            delete[] threads;
+
+        } break;
+
+        case OMP: {
+            // std::cout << "[USING OPENMP]" << std::endl;
+            // printf("thread: %d\n", threadNum);
+            int i;
+#pragma omp parallel for shared(agents) num_threads(threadNum) schedule(static)
+            for (i = 0; i < agentSize; i++) {
+                // int nthrds = omp_get_num_threads();
+                // printf("omp threads: %d\n", nthrds);
+                a1Move(agents[i]);
+            }
+
+        } break;
 
         default:
             break;
@@ -160,54 +154,55 @@ void Ped::Model::tick() {
 
 // Moves the agent to the next desired position. If already taken, it will
 // be moved to a location close to it.
-void Ped::Model::move(Ped::Tagent *agent)
-{
-	// Search for neighboring agents
-	set<const Ped::Tagent *> neighbors = getNeighbors(agent->getX(), agent->getY(), 2);
+void Ped::Model::move(Ped::Tagent* agent) {
+    // Search for neighboring agents
+    set<const Ped::Tagent*> neighbors =
+        getNeighbors(agent->getX(), agent->getY(), 2);
 
-	// Retrieve their positions
-	std::vector<std::pair<int, int> > takenPositions;
-	for (std::set<const Ped::Tagent*>::iterator neighborIt = neighbors.begin(); neighborIt != neighbors.end(); ++neighborIt) {
-		std::pair<int, int> position((*neighborIt)->getX(), (*neighborIt)->getY());
-		takenPositions.push_back(position);
-	}
+    // Retrieve their positions
+    std::vector<std::pair<int, int> > takenPositions;
+    for (std::set<const Ped::Tagent*>::iterator neighborIt = neighbors.begin();
+         neighborIt != neighbors.end(); ++neighborIt) {
+        std::pair<int, int> position((*neighborIt)->getX(),
+                                     (*neighborIt)->getY());
+        takenPositions.push_back(position);
+    }
 
-	// Compute the three alternative positions that would bring the agent
-	// closer to his desiredPosition, starting with the desiredPosition itself
-	std::vector<std::pair<int, int> > prioritizedAlternatives;
-	std::pair<int, int> pDesired(agent->getDesiredX(), agent->getDesiredY());
-	prioritizedAlternatives.push_back(pDesired);
+    // Compute the three alternative positions that would bring the agent
+    // closer to his desiredPosition, starting with the desiredPosition itself
+    std::vector<std::pair<int, int> > prioritizedAlternatives;
+    std::pair<int, int> pDesired(agent->getDesiredX(), agent->getDesiredY());
+    prioritizedAlternatives.push_back(pDesired);
 
-	int diffX = pDesired.first - agent->getX();
-	int diffY = pDesired.second - agent->getY();
-	std::pair<int, int> p1, p2;
-	if (diffX == 0 || diffY == 0)
-	{
-		// Agent wants to walk straight to North, South, West or East
-		p1 = std::make_pair(pDesired.first + diffY, pDesired.second + diffX);
-		p2 = std::make_pair(pDesired.first - diffY, pDesired.second - diffX);
-	}
-	else {
-		// Agent wants to walk diagonally
-		p1 = std::make_pair(pDesired.first, agent->getY());
-		p2 = std::make_pair(agent->getX(), pDesired.second);
-	}
-	prioritizedAlternatives.push_back(p1);
-	prioritizedAlternatives.push_back(p2);
+    int diffX = pDesired.first - agent->getX();
+    int diffY = pDesired.second - agent->getY();
+    std::pair<int, int> p1, p2;
+    if (diffX == 0 || diffY == 0) {
+        // Agent wants to walk straight to North, South, West or East
+        p1 = std::make_pair(pDesired.first + diffY, pDesired.second + diffX);
+        p2 = std::make_pair(pDesired.first - diffY, pDesired.second - diffX);
+    } else {
+        // Agent wants to walk diagonally
+        p1 = std::make_pair(pDesired.first, agent->getY());
+        p2 = std::make_pair(agent->getX(), pDesired.second);
+    }
+    prioritizedAlternatives.push_back(p1);
+    prioritizedAlternatives.push_back(p2);
 
-	// Find the first empty alternative position
-	for (std::vector<pair<int, int> >::iterator it = prioritizedAlternatives.begin(); it != prioritizedAlternatives.end(); ++it) {
+    // Find the first empty alternative position
+    for (std::vector<pair<int, int> >::iterator it =
+             prioritizedAlternatives.begin();
+         it != prioritizedAlternatives.end(); ++it) {
+        // If the current position is not yet taken by any neighbor
+        if (std::find(takenPositions.begin(), takenPositions.end(), *it) ==
+            takenPositions.end()) {
+            // Set the agent's position
+            agent->setX((*it).first);
+            agent->setY((*it).second);
 
-		// If the current position is not yet taken by any neighbor
-		if (std::find(takenPositions.begin(), takenPositions.end(), *it) == takenPositions.end()) {
-
-			// Set the agent's position 
-			agent->setX((*it).first);
-			agent->setY((*it).second);
-
-			break;
-		}
-	}
+            break;
+        }
+    }
 }
 
 /// Returns the list of neighbors within dist of the point x/y. This
@@ -216,20 +211,22 @@ void Ped::Model::move(Ped::Tagent *agent)
 /// \return  The list of neighbors
 /// \param   x the x coordinate
 /// \param   y the y coordinate
-/// \param   dist the distance around x/y that will be searched for agents (search field is a square in the current implementation)
+/// \param   dist the distance around x/y that will be searched for agents
+/// (search field is a square in the current implementation)
 set<const Ped::Tagent*> Ped::Model::getNeighbors(int x, int y, int dist) const {
-
-	// create the output list
-	// ( It would be better to include only the agents close by, but this programmer is lazy.)	
-	return set<const Ped::Tagent*>(agents.begin(), agents.end());
+    // create the output list
+    // ( It would be better to include only the agents close by, but this
+    // programmer is lazy.)
+    return set<const Ped::Tagent*>(agents.begin(), agents.end());
 }
 
 void Ped::Model::cleanup() {
-	// Nothing to do here right now. 
+    // Nothing to do here right now.
 }
 
-Ped::Model::~Model()
-{
-	std::for_each(agents.begin(), agents.end(), [](Ped::Tagent *agent){delete agent;});
-	std::for_each(destinations.begin(), destinations.end(), [](Ped::Twaypoint *destination){delete destination; });
+Ped::Model::~Model() {
+    std::for_each(agents.begin(), agents.end(),
+                  [](Ped::Tagent* agent) { delete agent; });
+    std::for_each(destinations.begin(), destinations.end(),
+                  [](Ped::Twaypoint* destination) { delete destination; });
 }
